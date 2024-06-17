15791d02a0b6f67b68c1f672cb4f5c123fa727ec
scova <scova@publicisgroupe.net>
2024-06-17 09:57:33 +0200

    38. Custom Query Hook: useTreatments

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -4,10 +4,10 @@ import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+ async function getTreatments(): Promise<Treatment[]> {
+   const { data } = await axiosInstance.get('/treatments');
+   return data;
+ }
 
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
