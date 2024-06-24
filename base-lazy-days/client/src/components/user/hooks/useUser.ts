49b5483d9a7db03283369a32b932e445f9c05014
scova <scova@publicisgroupe.net>
2024-06-24 13:12:12 +0200

    62. Add useQuery call to useUser

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
@@ -5,22 +6,32 @@ import type { User } from "@shared/types";
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { generateUserKey } from "@/react-query/key-factories";
 
 // query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${userId}`,
+    {
+      headers: getJWTHeader(userToken),
+    }
+  );
+  return data.user;
+}
 
-//   return data.user;
-// }
-
+// 62. Add useQuery call to useUser
 export function useUser() {
-  // TODO: call useQuery to update user data from server
-  const user: User = null;
+
+  // Get details on the userId
+  const { userId, userToken } = useLoginData();
+
+  // Call useQuery to update user data from server
+  const { data: user } = useQuery({
+    enabled: !!userId,
+    queryKey: generateUserKey(userId, userToken),
+    queryFn: () => getUser(userId, userToken),
+    staleTime: Infinity,
+  });
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
